82c42f387a9d0a4e5ffc20b9129b971e5be36eb9
katainaka0503 <katainaka0503@gmail.com>
2017-08-01 13:14:20 +0900

    TodoControllerの実装

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -2,15 +2,32 @@ package controllers
 
 import javax.inject._
 
-import model.Todo
 import model.Todo.autoSession
-import play.api._
+import model.{Id, Todo}
+import play.api.libs.json.Json
 import play.api.mvc._
-import scalikejdbc.DBSession
+import scalikejdbc.{DB, DBSession}
 
 @Singleton
 class TodoController @Inject()(todoDao: TodoDao, cc: ControllerComponents) extends AbstractController(cc) {
+  implicit def idFormat[A] = Json.format[Id[A]]
 
+  implicit val todoFormat = Json.format[Todo]
+
+
+  def findAll() = Action { implicit request =>
+    val all = DB.readOnly { implicit session =>
+      todoDao.findAll()
+    }
+    Ok(Json.toJson(all))
+  }
+
+  def findAllByKeyword(keyword: String) = Action { implicit request =>
+    val found = DB.readOnly { implicit session =>
+      todoDao.findAllByKeyword(keyword)
+    }
+    Ok(Json.toJson(found))
+  }
 }
 
 
--- test/controllers/TodoControllerSpec.scala
+++ test/controllers/TodoControllerSpec.scala
@@ -1,10 +1,60 @@
 package controllers
 
-import org.scalatestplus.play._
-import org.scalatestplus.play.guice._
-import play.api.test._
+import model.{Id, Todo}
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers._
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+import play.api.mvc.{ControllerComponents, Result}
 import play.api.test.Helpers._
+import play.api.test._
+import scalikejdbc.config.DBs
 
-class TodoControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+import scala.concurrent.Future
 
+class TodoControllerSpec extends FlatSpec with BeforeAndAfter with Matchers with MockitoSugar {
+  behavior of "TodoController"
+
+  DBs.setupAll()
+
+  val stubCC: ControllerComponents = Helpers.stubControllerComponents()
+  val mockDao: TodoDao = mock[TodoDao]
+
+  val controller = new TodoController(mockDao, stubCC)
+  import controller.todoFormat
+
+  after{
+    reset(mockDao)
+  }
+
+  it should "findAllTodos" in {
+    val todos = List(Todo(Id(1), "title1", "desc1"), Todo(Id(2), "title2", "desc2"))
+    when(mockDao.findAll()(any())).thenReturn(todos)
+
+    val result: Future[Result] = controller.findAll().apply(FakeRequest())
+
+    status(result) should be (200)
+    contentAsJson(result).as[List[Todo]] should be(todos)
+  }
+
+  it should "findNothing when todo is not registered" in {
+    when(mockDao.findAll()(any())).thenReturn(Seq.empty)
+
+    val result: Future[Result] = controller.findAll().apply(FakeRequest())
+
+    status(result) should be (200)
+    contentAsJson(result).as[List[Todo]] should be(Seq.empty)
+  }
+
+  it should "findByKeyword" in {
+    val keyword = "keyword"
+    val found = List(Todo(Id(1), keyword, "desc1"), Todo(Id(2), "title2", keyword))
+    when(mockDao.findAllByKeyword(ArgumentMatchers.eq(keyword))(any())).thenReturn(found)
+
+    val result: Future[Result] = controller.findAllByKeyword(keyword).apply(FakeRequest())
+
+    status(result) should be (200)
+    contentAsJson(result).as[List[Todo]] should be(found)
+  }
 }
